ffacdb6e7d0e14f9f4fc2d28540507758bf65c0f
wenluanfei <160090289+wenluanfei@users.noreply.github.com>
2025-05-25 20:03:38 +1000

    change about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,10 @@ function About() {
   return (
     <div className="section" id="about" data-aos="fade-up">
       <h2>About Me</h2>
-      <p>I enjoy building responsive and interactive websites...</p>
+      <p>
+        I'm a passionate web developer with experience in building responsive and dynamic websites using modern tools like React. I love clean design, efficient code, and creating seamless user experiences. Currently open to exciting opportunities and collaborations!
+      </p>
+
     </div>
   );
 }
