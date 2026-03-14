c48fb0192c9a7306da1f98d09135cf80f549bdbb
wenluanfei <160090289+wenluanfei@users.noreply.github.com>
2026-03-14 13:33:31 +1000

    fix: remove BOM, fix cursor, remove unused var

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import "./styles/App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
